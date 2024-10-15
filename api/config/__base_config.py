641582d503ac7f1ec3b31ac151b853e9b5223262
Sarctiann (Sebastian Rodriguez) <sebastian.rodriguez@hstpathways.com>
2024-10-15 12:34:04 -0300

    Updated env variables

--- api/config/__base_config.py
+++ api/config/__base_config.py
@@ -2,9 +2,8 @@ __all__ = [
     "MONGODB_URI",
     "logger",
     "SECRET_KEY",
-    "HOST_URL",
-    "HOST_PORT",
-    "FRONTEND_HOST",
+    "BACKEND_DOMAIN",
+    "FRONTEND_DOMAIN",
 ]
 
 import logging
@@ -23,15 +22,14 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 if not SECRET_KEY:
     raise Exception("Secret key not found")
 
-HOST_URL = os.getenv("HOST_URL")
-if not HOST_URL:
-    raise Exception("Host url not found")
+BACKEND_DOMAIN = os.getenv("BACKEND_DOMAIN")
+if not BACKEND_DOMAIN:
+    raise Exception("Backend domain not found")
 
-FRONTEND_HOST = os.getenv("FRONTEND_HOST")
-if not FRONTEND_HOST:
-    raise Exception("Frontend host not found")
+FRONTEND_DOMAIN = os.getenv("FRONTEND_DOMAIN")
+if not FRONTEND_DOMAIN:
+    raise Exception("Frontend domain not found")
 
-HOST_PORT = int(os.getenv("HOST_PORT") or 8000)
 
 logger = logging.getLogger("uvicorn")
 # logger.setLevel(logging.DEBUG)
